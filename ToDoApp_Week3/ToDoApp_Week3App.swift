40f91ca2de1587c20ec6858dea8f0608587372e4
JanhaviJagtap <jagtapjanhavi2000@gmail.com>
2025-08-13 15:26:19 +1000

    Initial Commit

---
+++ ToDoApp_Week3/ToDoApp_Week3App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_Week3App.swift
+//  ToDoApp_Week3
+//
+//  Created by Janhavi Jagtap on 13/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp_Week3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoApp_Week3Tests/ToDoApp_Week3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_Week3Tests.swift
+//  ToDoApp_Week3Tests
+//
+//  Created by Janhavi Jagtap on 13/8/2025.
+//
+
+import Testing
+@testable import ToDoApp_Week3
+
+struct ToDoApp_Week3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
